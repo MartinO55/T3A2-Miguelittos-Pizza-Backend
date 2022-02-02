d7aca8cec2e2cdf897b1d6f8bc8391e8785305f2
Mat-Carv <mat.candido2012@gmail.com>
2022-02-02 17:12:44 +1100

    Added client.http for request testing using REST client. Added admin: to Users

--- MiguelittosPizza/config/routes.rb
+++ MiguelittosPizza/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/", to: "pizzas#index"
+  get '/', to: 'pizzas#index'
+  
+  scope '/auth' do
+    post '/sign_up', to: 'users#create'
+  end
 end
--- MiguelittosPizza/db/seeds.rb
+++ MiguelittosPizza/db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: "Miguel", email: "miguel@test.com", password: "abcdef", password_confirmation: "abcdef")
+User.create(name: "Miguel", email: "miguel@test.com", password: "abcdef", password_confirmation: "abcdef", admin: true)
 
 categories = [
     "Bases",
